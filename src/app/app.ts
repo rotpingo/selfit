4636e958e4755e51714d634bb309971c8791781b
rotpingo <viorel.moschoi@gmail.com>
2025-06-03 16:19:38 +0200

    some global css styling, new nav component + some styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBar } from './components/top-bar/top-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
