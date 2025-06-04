45d2c814a62fd54288e9ec7c3f7cdf946f589de5
rotpingo <viorel.moschoi@gmail.com>
2025-06-04 14:22:25 +0200

    created notes and home components, added routes and some styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Notes } from './components/notes/notes';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'notes',
+    component: Notes,
+  }
+];
--- src/app/components/side-bar/side-bar.ts
+++ src/app/components/side-bar/side-bar.ts
@@ -1,11 +1,17 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './side-bar.html',
   styleUrl: './side-bar.css'
 })
 export class SideBar {
   isOpen = input<boolean>(false);
+  navigate = output<void>();
+
+  onNavigate() {
+    this.navigate.emit();
+  }
 }
