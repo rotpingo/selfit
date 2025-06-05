d00b734ebceef22cb098e8290ac459e7cb144709
rotpingo <viorel.moschoi@gmail.com>
2025-06-05 13:26:17 +0200

    removed the dummy data

--- src/app/data/db.ts
+++ src/app/data/db.ts
@@ -1,35 +1,6 @@
 import Dexie, { Table } from "dexie";
 import { Note } from "../model/note.model";
 
-export const dummyNotes: Note[] = [
-  {
-    id: 1,
-    title: 'Grocery List',
-    content: 'Milk, Eggs, Bread, Coffee',
-    createdAt: Date.now() - 1000000,
-  },
-  {
-    id: 2,
-    title: 'Workout Plan',
-    content: 'Monday: Chest\nTuesday: Back\nWednesday: Legs',
-    createdAt: Date.now() - 500000,
-  },
-  {
-    id: 3,
-    title: 'Angular Notes',
-    content: 'Explore signals, resources, and IndexedDB',
-    createdAt: Date.now() - 10000,
-  },
-];
-
-export async function seedDummyNotes() {
-  const count = await db.notes.count();
-  if (count === 0) {
-    await db.notes.bulkAdd(dummyNotes);
-    console.log('Dummy notes seeded!');
-  }
-}
-
 export class AppDB extends Dexie {
 
   notes!: Table<Note>;
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { seedDummyNotes } from './app/data/db';
 
-seedDummyNotes();
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
