207c429a997b60964eb7fb20ae50dfa69a72e2f1
rotpingo <viorel.moschoi@gmail.com>
2025-06-24 12:24:47 +0200

    changed task model

--- src/app/model/task.model.ts
+++ src/app/model/task.model.ts
@@ -1,6 +1,13 @@
 export interface TaskModel {
   id?: number;
+  parentId: number;
   title: string;
   content: string;
+  isRepeat: boolean;
+  interval?: number;
+  status: 'done' | 'canceled' | 'progress';
+  notes?: string;
+  execDate: Date;
   createdAt?: Date;
+  updatedAt?: Date;
 }
