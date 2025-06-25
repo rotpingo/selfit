4e89477c4f691c30757b70ac69c3997f124c6723
rotpingo <viorel.moschoi@gmail.com>
2025-06-25 13:35:58 +0200

    added new shared component btn

---
+++ src/app/components/shared/fixed-btn/fixed-btn.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fixed-btn',
+  imports: [],
+  templateUrl: './fixed-btn.html',
+  styleUrl: './fixed-btn.css'
+})
+export class FixedBtn {
+
+}
